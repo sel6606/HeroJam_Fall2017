75a80c10212d8604525f92c09f57dbeaa5c12c1b
msl5994 <msl5994@g.rit.edu>
2017-10-28 17:42:44 -0400

    Spawns animals and destorys them when touched. Doesn't spawn an animal on a space with fire or on a space that already has an animal on it.

---
+++ HeroJamProject/Assets/Scripts/SpawnAnimals.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAnimals : MonoBehaviour {
+
+    // take a reference to a prefab to spawn
+    public GameObject animal;
+
+    // get a reference to the forest cell positions
+    public GameObject forestMnger;
+    private ForestGenerator forestGen;
+
+    private ForestCell spawnCell;
+
+    // create intVec2's for random placement
+    private float xSpawn;
+    private float ySpawn;
+    IntVector2 spawnPos;
+
+    // create a list of intVector2's to keep track of current spawn positions of animals
+    // and make sure that none of them are the same
+    List<IntVector2> animalSpawns;
+
+    private bool samePos = false;
+
+    public int animalCount = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // referece to forestGen script
+        forestGen = forestMnger.GetComponent<ForestGenerator>();
+
+        // instantiate the list
+        animalSpawns = new List<IntVector2>();
+        /*
+        for (int i = 0; i < animalCount; i++)
+        {
+            SpawnAnimal();
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        // spawn animals
+        if(animalCount<20)
+        {
+            SpawnAnimal();
+        }
+    }
+
+    void SpawnAnimal()
+    {
+        // generate random ints
+        xSpawn = Random.Range(0, forestGen.sizeX);
+        ySpawn = Random.Range(0, forestGen.sizeX);
+
+        // create the spawn position
+        spawnPos = new IntVector2((int)xSpawn, (int)ySpawn);
+
+        // get the cell in which the animal should spawn
+        spawnCell = forestGen.GetCell(spawnPos);
+
+        // check to make sure the position is unique
+        for(int i = 0; i < animalSpawns.Count; i++)
+        {
+            if (spawnPos == animalSpawns[i])
+            {
+                samePos = true;
+            }
+        }
+
+        // add it to the list of spawnPositions
+        animalSpawns.Add(spawnPos);
+
+        if (samePos == false)
+        {
+            // check to make sure the tile isn't already on fire
+            if (spawnCell.OnFire == false)
+            {
+                // get the world position of the forest cell
+                Vector3 tempSpawn = new Vector3(spawnCell.transform.position.x, spawnCell.transform.position.y + 0.2f, spawnCell.transform.position.z);
+
+                // instantiate the object in the world on the cell
+                Instantiate(animal, tempSpawn, Quaternion.identity);
+
+                // increment the animal spawn counter
+                animalCount++;
+            }
+        }
+    }
+}
